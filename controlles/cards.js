d85dd89a5fbf07737ab537b2fd3642ae0a006a1b
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-10-12 13:45:09 +0300

    changed cards js
    added return so the fun will stop sending header after first header was sent. & delete condition

--- controlles/cards.js
+++ controlles/cards.js
@@ -18,6 +18,7 @@ const createCard = (req, res) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        return;
       }
       res.status(500).send({ message: 'Ошибка по умолчинию' });
     });
@@ -34,7 +35,7 @@ const deleteCard = (req, res) => {
       res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
+      if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные для удаления карточки.' });
       } else {
         res.status(500).send({ message: 'Ошибка по умолчинию' });
