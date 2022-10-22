17fd95b308c685864800fb7c9c5bcc33d6263173
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-10-22 11:23:05 +0300

    changed user model
    regexp validation
    hidden password

--- models/user.js
+++ models/user.js
@@ -5,17 +5,33 @@ const userSchema = new mongoose.Schema({
     type: String,
     maxlength: 30,
     minlength: 2,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     maxlength: 30,
     minlength: 2,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.text(v);
+      },
+      message: 'Переданы некорректные данные',
+    },
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
     required: true,
+    select: false,
   },
 });
 
