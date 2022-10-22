e69bf08a9219cf386a9914ddc6a15168341f0edc
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-10-22 11:22:27 +0300

    create middleware
    this middleware protecs some routes from unauth actibitiest

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { cookie } = req.headers;
+  if (!cookie || !cookie.startsWith('Bearer')) {
+    return res.status(401).send({ message: 'Необходимо авторизироваться' });
+  }
+  const token = cookie.replace('Bearer=', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(401).send({ message: 'Необходима авторизироваться' });
+  }
+  req.user = payload;
+  next();
+};
