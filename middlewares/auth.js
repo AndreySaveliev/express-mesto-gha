87a66e95a4c9e01f912256d67fbbccf4e301e027
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-10-23 13:49:32 +0300

    now throwing error to handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
+const Error401 = require('../Errors/Error401');
 
 const { JWT_SECRET = 'YANDEX' } = process.env;
 
 module.exports = (req, res, next) => {
   const { cookie } = req.headers;
   if (!cookie || !cookie.startsWith('Bearer')) {
-    return res.status(401).send({ message: 'Необходимо авторизироваться' });
+    throw new Error401('Необходимо авторизироваться');
   }
   const token = cookie.replace('Bearer=', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизироваться' });
+    throw new Error401('Необходимо авторизироваться');
   }
   req.user = payload;
   next();
