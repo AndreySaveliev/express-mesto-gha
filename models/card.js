3923add865cebccd5b67e94e9e0ab8cf53f8cca5
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-10-23 19:44:43 +0300

    changed regexp
    now regexp will allow all valir urls

--- models/card.js
+++ models/card.js
@@ -13,7 +13,7 @@ const cardSchema = new mongoose.Schema({
     validate: {
       validator(v) {
         if (v) {
-          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+~#?&/=]*)/.test(v);
+          /https?:\/\/(www\.)?[-a-zA-Z0-9]{1,256}\.[a-zA-Z0-9]{1,6}[a-zA-Z0-9._~:/?#[\]@!$&()*+,;=-]{1,256}$/.test(v);
         }
       },
     },
