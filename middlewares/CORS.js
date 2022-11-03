8b1178cbea1ecb337374aa699f751492b2f77deb
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-03 17:24:48 +0300

    Update CORS.js

--- middlewares/CORS.js
+++ middlewares/CORS.js
@@ -1,19 +1,10 @@
-const allowedDom = [
-  'http://saveliev.nomoredomains.icu',
-];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
   const requestHeaders = req.headers['Content-Type, Authorization'];
   // eslint-disable-next-line no-bitwise
-  if (allowedDom.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    if (method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-    }
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  res.header('Access-Control-Allow-Headers', requestHeaders);
   next();
 };
