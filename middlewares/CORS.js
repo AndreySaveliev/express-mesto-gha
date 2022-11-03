38c52cbc9460e1a6e064fa5ad10c8311c2a12cd5
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-03 16:30:37 +0300

    cors fin

--- middlewares/CORS.js
+++ middlewares/CORS.js
@@ -3,6 +3,8 @@ const allowedDom = [
 ];
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
   if (allowedDom.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
