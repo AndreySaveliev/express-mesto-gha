cd93e817ffcc58096b862c40c814d772a49f21d3
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-03 17:28:46 +0300

    Update CORS.js

--- middlewares/CORS.js
+++ middlewares/CORS.js
@@ -4,7 +4,11 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['Content-Type, Authorization'];
   // eslint-disable-next-line no-bitwise
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  res.header('Access-Control-Allow-Headers', requestHeaders);
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
   next();
 };
